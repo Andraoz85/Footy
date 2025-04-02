56c6b0ffeb4befc06d60b0a0b729330d6765c059
Andraoz85 <andraoz85@gmail.com>
2025-04-02 11:46:59 +0200

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import Navbar from "./Navbar";
+
+export default function Header() {
+  return (
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            {/* Logo/Brand? */}
+            <div className="flex items-center">
+              <h1 className="text-xl font-bold">Footy</h1>
+            </div>
+            {/* Navigation Links */}
+            <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+              <a
+                href="/"
+                className="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Fixtures
+              </a>
+              <a
+                href="/tables"
+                className="border-transparent text-gray-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Tables
+              </a>
+              <a
+                href="/players"
+                className="border-transparent text-gray-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Players
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
