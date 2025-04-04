73bef0f56fcfb6404b628eae58dd1c03127e8a3e
Andraoz85 <andraoz85@gmail.com>
2025-04-04 11:39:29 +0200

    moved layout-components to subcategory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Sidebar from "@/components/Sidebar";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import Sidebar from "@/components/layout/Sidebar";
 
 export default function Home() {
   return (
